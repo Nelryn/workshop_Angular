3ccf33f8c67752ffa22fa6cf9d735aa02fd321f2
Jojolions <emilie.baunifais@epitech.eu>
2023-01-31 16:51:11 +0400

    here is my workshop

---
+++ WorkshopAngular/src/app/product-displayer/product-displayer.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-displayer',
+  templateUrl: './product-displayer.component.html',
+  styleUrls: ['./product-displayer.component.css']
+})
+export class ProductDisplayerComponent {
+
+  @Input() products! : Product[];
+
+  decrementStock(item : Product) {
+    item.stock -= 1;
+  }
+
+}
